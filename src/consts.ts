b95f1ae6c1b047524a3293a379552098a744b053
p <44508091+mrmotes@users.noreply.github.com>
2025-07-23 15:03:02 -0500

    minor content adjustments

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "cpl.sk8";
+export const SITE_DESCRIPTION = "welcome to cpl.sk8";
